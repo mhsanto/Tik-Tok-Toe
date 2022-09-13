cf7951e870cd7b5d96d3a54eb8a9c0f5c7300ef0
Mahmodul Hasan <nicesanto92@gmail.com>
2022-09-13 20:16:11 +0600

    complete game

--- script.js
+++ script.js
@@ -1,33 +1,57 @@
 let turn = 'X';
+let gameOver = false;
+let boxTexts = document.getElementsByClassName('boxtext');
+let button = document.querySelector('button');
 // function to change the turn
-const changeTurn = ()=>{
+const changeTurn = () => {
     return turn === "X" ? "O" : "X";
 }
 // function to check for a win
-const checkWin = ()=>{
-    let boxText = document.querySelectorAll('.boxtext');
+const checkWin = () => {
+    
     let wins = [
-        [1,2,3],
-        [4,5,6],
-        [7,8,9],
-        [1,4,7],
-        [2,5,8],
-        [3,6,9],
-        [1,5,9],
-        [3,5,7],
-        
-    ]
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6],
+    ];
+    wins.forEach(e => {
+        if ((boxTexts[e[0]].innerText === boxTexts[e[1]].innerText) && (boxTexts[e[2]].innerText === boxTexts[e[1]].innerText) && (boxTexts[e[0]].innerText !== "")) {
+            document.querySelector('.turnIndicator').innerText = boxTexts[e[0]].innerText + " Won ";
+            gameOver = true;
+             document.querySelector('.turnIndicator').style.color = "Green";
+        }
+    })
 }
 // game logic
 let boxes = document.getElementsByClassName("square");
-Array.from(boxes).forEach(element =>{
+Array.from(boxes).forEach(element => {
     let boxText = element.querySelector('.boxtext');
-    element.addEventListener('click',()=>{
-        if(boxText.innerText === ''){
+    element.addEventListener('click', () => {
+        if (boxText.innerText === '') {
             boxText.innerText = turn;
             turn = changeTurn();
             checkWin();
-            document.querySelector('.turnIndicator').innerText = turn + "'s Turn";
+            if (!gameOver) {
+                document.querySelector('.turnIndicator').innerText = turn + "'s Turn";
+               
+            }
         }
     })
+})
+
+// reset game 
+button.addEventListener('click',()=>{
+    Array.from(boxTexts).forEach(e=>{
+       e.innerText = "";
+     
+    })
+    turn ='X';
+    document.querySelector('.turnIndicator').innerText = turn + "'s Turn";
+    gameOver = false;
+    document.querySelector('.turnIndicator').style.color = "red";
 })
